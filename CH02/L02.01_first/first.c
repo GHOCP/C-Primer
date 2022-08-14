7adf316387f589626dc2cb1f89718d54e215d7c9
GHOCP <bbbbocp@163.com>
2022-08-14 21:48:46 +0800

    vscode + gcc

--- CH02/L02.01_first/first.c
+++ CH02/L02.01_first/first.c
@@ -1,10 +1,8 @@
 /* a simple program showing the basics of a C program */
 #include <stdio.h>
 int main (void) {               // a simple program
-
     int num;                    // define a variable named num
     num = 1;                    // assign a value to num
-
     printf("I am a simple ");   // use the printf() function
     printf("computer. \n");
     printf("My favorite number is %d because it is first.\n",num);
