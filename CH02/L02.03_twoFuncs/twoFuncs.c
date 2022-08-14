7ad1f29d7b6841fef1c85063bbee8d3833a4436c
GHOCP <bbbbocp@163.com>
2022-08-14 21:53:07 +0800

    vscode + gcc

--- CH02/L02.03_twoFuncs/twoFuncs.c
+++ CH02/L02.03_twoFuncs/twoFuncs.c
@@ -3,11 +3,9 @@
 void butler(void);        // ANSI/ISO C function prototyping
 
 int main(void) {
-
     printf("I will summon the butler function.\n");
     butler();
     printf("Yes. Bring me some tea and writeable DVDs.\n");
-
     return 0;
 }
 
